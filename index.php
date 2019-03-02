81c71e9ad78b0ac5b75d0b6b6eb55c485fccfc8f
Alex00Sam <alsa00@mail.ru>
2019-03-02 10:41:20 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
-  $h = fopen("population_number_sex_age_8.csv", "r");
-  //$data = fgetcsv($h, 1000, ",");
+  $h = fopen("test.csv", "r");
+//  $data = fgetcsv($h, 1000, ",");
   while (($data = fgetcsv($h, 1000, ",")) !== FALSE)
   {
+    
     $a[] = $data;
   }
   fclose($h);
